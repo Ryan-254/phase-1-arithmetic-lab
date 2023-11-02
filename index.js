0a81abae39951b985288c598861314f42969b1f3
Ryan Mwangi Kamau <kamrymwangi@gmail.com>
2023-11-02 14:50:50 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+// Multiply
+var num1 = 2;
+var num2 = 31;
+var multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+// Random
+let random = 0;
+random ++;
+console.log(random);
+
+// mod
+var num3 = 9;
+var num4 = 18;
+var mod = num3 % num4;
+console.log(mod); 
+
+// max
+var numbers = [13, 10, 4, 1, 20, 9];
+var max = Math.max(...numbers);
+console.log(max)
